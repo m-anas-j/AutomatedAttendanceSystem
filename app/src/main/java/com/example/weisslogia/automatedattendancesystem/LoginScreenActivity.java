ff13758f44b618fe6a9d1a6f0728a469bcdbb263
Ifaz Ahmed Aflan <ifaz.mobile.dev@gmail.com>
2018-04-23 23:06:59 +0600

    added student login screen

--- app/src/main/java/com/example/weisslogia/automatedattendancesystem/LoginScreenActivity.java
+++ app/src/main/java/com/example/weisslogia/automatedattendancesystem/LoginScreenActivity.java
@@ -1,7 +1,9 @@
 package com.example.weisslogia.automatedattendancesystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginScreenActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class LoginScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+    }
+
+    public void gotoTransferringStudentId(View view) {
+        Intent transferringStudId = new Intent(this, TransferringStudentID.class);
+        startActivity(transferringStudId);
     }
 }
